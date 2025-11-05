13370df7fea159dd0533f7bb470e7cfbd7fd5f7a
tomas7L <buffatomy06@gmail.com>
2025-11-05 11:11:39 -0300

    Esqueleto

---
+++ ClasesTercerParcial/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClasesTercerParcial.Models;
+using Microsoft.EntityFrameworkCore;
+namespace ClasesTercerParcial.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Empleado> Empleados { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=TOMµS;Database=Parcial3DB;Trusted_Connection=True;TrustServerCertificate=True;"
+            );
+        }
+    }
+}
---
+++ FRONT/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace FRONT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool salir = false;
+
+            while (!salir)
+            {
+                Console.Clear();
+                Console.WriteLine("Menú:");
+                Console.WriteLine("1- Registrar libro");
+                Console.WriteLine("2- Registrar préstamo");
+                Console.WriteLine("3- Mostrar información de un libro");
+                Console.WriteLine("4- Mostrar todos los libros y sus estadísticas");
+                Console.WriteLine("5- Salir");
+
+                string opcion = Console.ReadLine().Trim();
+
+                switch (opcion)
+                {
+                    case "1":
+                        // Registrar libro
+                        Console.ReadKey(true);
+                        break;
+
+                    case "2":
+                        // Registrar préstamo
+                        Console.ReadKey(true);
+                        break;
+
+                    case "3":
+                        // Mostrar información de un libro
+                        Console.ReadKey(true);
+                        break;
+
+                    case "4":
+                        // Mostrar todos los libros y sus estadísticas
+                        Console.ReadKey(true);  
+                        break;
+
+                    case "5":
+                        salir = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opción incorrecta");
+                        Console.ReadKey(true);
+                        break;
+                }
+            }
+        }
+    }
+}
+
